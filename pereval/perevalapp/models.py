3f019cfec28e5ac6f59e0291b7847c848148fa75
Igor39-dev <19gorokhov90@gmail.com>
2025-10-17 02:18:22 +0200

    created DB (postgreSQL)
    installed psycopg2
    installed dotenv
    .env.example

--- pereval/perevalapp/models.py
+++ pereval/perevalapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+# {
+#   "beauty_title": "пер. ",
+#   "title": "Пхия",
+#   "other_titles": "Триев",
+#   "connect": "", // что соединяет, текстовое поле
+ 
+#   "add_time": "2021-09-22 13:18:13",
+#   "user": {"email": "qwerty@mail.ru", 		
+#         "fam": "Пупкин",
+# 		 "name": "Василий",
+# 		 "otc": "Иванович",
+#         "phone": "+7 555 55 55"}, 
+ 
+#    "coords":{
+#   "latitude": 45.3842,
+#   "longitude": 7.1525,
+#   "height": "1200"}
+ 
+ 
+#   level:{"winter": "", //Категория трудности. В разное время года перевал может иметь разную категорию трудности
+#   "summer": "1А",
+#   "autumn": "1А",
+#   "spring": ""},
+ 
+#    images: [{"data":"картинка1", "title":"Седловина"}, {"data":"картинка", "title":"Подъём"}]
+# }
