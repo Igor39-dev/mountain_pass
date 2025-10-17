3aafcae1236896449a43dc6a2a2a3ba8114b60aa
Igor39-dev <19gorokhov90@gmail.com>
2025-10-17 19:33:45 +0200

    Created ViewSets

--- pereval/perevalapp/views.py
+++ pereval/perevalapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from perevalapp.serializers import CoordsSerializer, ImagesSerializer, LevelSerializer, PerevalSerializer, UserSerializer
+from perevalapp.models import Coords, Images, Level, Pereval, User
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CoordsViewSet(ModelViewSet):
+    queryset = Coords.objects.all()
+    serializer_class = CoordsSerializer
+
+
+class LevelViewSet(ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+
+class PerevalViewSet(ModelViewSet):
+    queryset = Pereval.objects.all()
+    serializer_class = PerevalSerializer
+
+
+class ImagesViewSet(ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = ImagesSerializer
